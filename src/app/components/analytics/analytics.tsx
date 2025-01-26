2bc7dc9b24ae1b1af00161cdc873c205d8f6a7c4
vinayakfr <vinayak.sri@yahoo.com>
2025-01-27 01:23:27 +0530

    feat: create analytics component

---
+++ src/app/components/analytics/analytics.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import MultiLineChart from "./line";
+
+function Analytics() {
+  const sampleData = [
+    {
+      name: "Dataset 1",
+      values: [
+        { date: "2025-01-01", value: 30 },
+        { date: "2025-01-02", value: 50 },
+        { date: "2025-01-03", value: 90 },
+        { date: "2025-01-04", value: 70 },
+      ],
+    },
+    {
+      name: "Dataset 2",
+      values: [
+        { date: "2025-01-01", value: 40 },
+        { date: "2025-01-02", value: 20 },
+        { date: "2025-01-03", value: 60 },
+        { date: "2025-01-04", value: 80 },
+      ],
+    },
+    {
+      name: "Dataset 2",
+      values: [
+        { date: "2025-01-01", value: 10 },
+        { date: "2025-01-02", value: 40 },
+        { date: "2025-01-03", value: 70 },
+        { date: "2025-01-04", value: 30 },
+      ],
+    },
+  ];
+
+  const sampleData2 = [
+    {
+      name: "Dataset 1",
+      values: [
+        { date: "2025-01-01", value: 45 },
+        { date: "2025-01-02", value: 80 },
+        { date: "2025-01-03", value: 34 },
+        { date: "2025-01-04", value: 78 },
+      ],
+    },
+    {
+      name: "Dataset 2",
+      values: [
+        { date: "2025-01-01", value: 50 },
+        { date: "2025-01-02", value: 67 },
+        { date: "2025-01-03", value: 87 },
+        { date: "2025-01-04", value: 40 },
+      ],
+    },
+    {
+      name: "Dataset 2",
+      values: [
+        { date: "2025-01-01", value: 10 },
+        { date: "2025-01-02", value: 40 },
+        { date: "2025-01-03", value: 70 },
+        { date: "2025-01-04", value: 30 },
+      ],
+    },
+  ];
+
+  return (
+    <div className="flex place-content-center place-items-center w-full">
+      <div className="flex items-center justify-between w-[99%]">
+        <div className="flex-col place-content-center place-items-center">
+          <div className="flex place-content-center place-items-center p-2 rounded-xl w-auto shadow-lg">
+            <MultiLineChart data={sampleData} />
+          </div>
+          <div className="flex place-content-center place-items-center p-2 rounded-xl w-auto shadow-lg">
+            <MultiLineChart data={sampleData2} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Analytics;
