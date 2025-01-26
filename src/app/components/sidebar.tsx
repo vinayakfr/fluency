02478e22052c66a325aa912d6cd8d874f5f80fa2
vinayakfr <vinayak.sri@yahoo.com>
2025-01-27 01:18:02 +0530

    modify sidebar

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -8,40 +8,44 @@ import { IoIosHelpCircleOutline } from "react-icons/io";
 
 function Sidebar() {
   return (
-    <div className="flex flex-col gap-20 place-content-start place-items-center bg-transparent pr-5 pl-2 py-24">
-      <div className="grid grid-cols-1 grid-rows-3 gap-y-10 items-center">
+    <div className="flex flex-col gap-20 place-content-start place-items-center bg-transparent pr-4 pl-4 py-24 !bg-black rounded-2xl">
+      <div className="grid grid-cols-1 grid-rows-3 gap-y-10 place-content-center items-center">
         <a href="/" className="group flex items-center">
           <LuLayoutDashboard size={40} color="white" />
+          {/* <LuLayoutDashboard size={40} color="white" className="hidden dark:block"/> */}
           <p
             className="absolute transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Dashboard
           </p>
         </a>
         <button className="group flex items-center">
           <MdOutlineDriveFolderUpload size={40} color="white" />
+          {/* <MdOutlineDriveFolderUpload size={40} color="white" className="hidden dark:block"/> */}
           <p
             className="absolute transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Upload
           </p>
         </button>
         <a href="/analytics" className="group flex items-center">
           <MdOutlineAnalytics size={40} color="white" />
+          {/* <MdOutlineAnalytics size={40} color="white" className="hidden dark:block"/> */}
           <p
             className="absolute transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Analytics
           </p>
         </a>
         <button className="group flex items-center">
           <MdOutlineCalendarMonth size={40} color="white" />
+          {/* <MdOutlineCalendarMonth size={40} color="whiblacke" className="hidden dark:block"/> */}
           <p
             className="absolute transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Calendar
           </p>
@@ -50,18 +54,20 @@ function Sidebar() {
       <div className="grid grid-cols-1 grid-rows-3 gap-y-10 items-center">
         <button className="group flex items-center">
           <IoMdSettings size={40} color="white" />
+          {/* <IoMdSettings size={40} color="white" className="hidden dark:block"/> */}
           <p
             className="absolute transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Settings
           </p>
         </button>
         <button className="group flex items-center">
           <IoIosHelpCircleOutline size={40} color="white" />
+          {/* <IoIosHelpCircleOutline size={40} color="white" className="hidden dark:block"/> */}
           <p
             className="absolute text-white font-semibold transition-all duration-300 opacity-0 group-hover:opacity-100 
-             bg-cyan-500 rounded-xl p-2 left-20"
+             bg-rose-500 rounded-xl p-2 left-20"
           >
             Help
           </p>
