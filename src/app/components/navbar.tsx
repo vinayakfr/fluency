48c9bd5602af007ba4ddb1d858a87f602674fd87
vinayakfr <vinayak.sri@yahoo.com>
2025-01-27 01:18:42 +0530

    chore: modify navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,15 +4,15 @@ function Navbar() {
   return (
     <div className="w-full">
       <nav className="flex items-center justify-between px-2 py-4 rounded-xl bg-transparent">
-        <button className="text-5xl font-bold text-black">
-          <span className="underline">fluen</span>cy
+        <button className="text-5xl font-bold text-white ">
+          <span className="underline text-rose-500">fluen</span>cy
         </button>
         <div className="flex items-center gap-5">
-          <p className="text-2xl font-medium text-black">Vinayak Srivastava</p>
+          <p className="text-2xl font-medium text-white ">Vinayak Srivastava</p>
           <img
             src="avatar.jpg"
             alt="profile"
-            className="rounded-full h-14 w-14 bg-black"
+            className="rounded-full h-14 w-14"
           />
         </div>
       </nav>
