e2d03e972797a703455b737a501abd35eb0b8f42
vinayakfr <vinayak.sri@yahoo.com>
2025-01-27 10:22:35 +0530

    feat: create index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Sidebar from "./components/sidebar";
+import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="flex flex-col xl:flex-row gap-3 p-2 bg-white h-full xl:h-screen">
+      <Sidebar />
+      <Hero />
     </div>
   );
 }
