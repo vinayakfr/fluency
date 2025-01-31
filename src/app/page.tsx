106ee821853c48ef790c42f380ceeadaf57d2ae2
vinayakfr <vinayak.sri@yahoo.com>
2025-01-31 08:43:58 +0530

    feat: add cosmetic changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Hero from "./components/hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col xl:flex-row gap-3 p-2 bg-white h-full xl:h-screen">
-      <div className="hidden xl:block w-full">
+    <div className="flex flex-col lg:flex-row gap-3 p-2 bg-white h-full xl:h-screen">
+      <div className="hidden lg:block">
         <Sidebar />
       </div>
       <Hero />
